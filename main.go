31a872a2b35feb0127bbda273a5f0b805935f612
okoenig <oliver.koenig@dpdhl.com>
2023-07-01 15:18:59 +0200

    refactor: Modularize code

--- main.go
+++ main.go
@@ -4,41 +4,7 @@ import (
 	"fmt"
 	"os"
 	"sort"
-	"strconv"
-	"strings"
 )
-
-func sum(listToSum []int) int {
-	var sum = 0
-	for _, item := range listToSum {
-		sum += item
-	}
-	return sum
-}
-
-func getCaloriesPerElve(file []byte) []int {
-	var calories []string = strings.Split(string(file), "\n")
-	var caloriesPerElve []int
-
-	var sumOfCalories int = 0
-	for _, calorie := range calories {
-
-		if calorie == "" {
-			caloriesPerElve = append(caloriesPerElve, sumOfCalories)
-			sumOfCalories = 0
-			continue
-		}
-
-		marks, err := strconv.Atoi(calorie)
-		if err != nil {
-			panic(err)
-		}
-
-		sumOfCalories += marks
-	}
-
-	return caloriesPerElve
-}
 
 func main() {
 	var path string = "/home/okoenig/private_code_projects/advent-of-code-2022-go/data/challenge.txt"
