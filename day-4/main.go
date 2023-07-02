62cf636235220922bf64df6bd3e011f2d2555a34
okoenig <oliver.koenig@dpdhl.com>
2023-07-02 15:52:45 +0200

    feat: Day-4 Part-2

--- day-4/main.go
+++ day-4/main.go
@@ -18,17 +18,27 @@ func newSection(section string) *Section {
 	return &Section{min: firstPair, max: secondPair}
 }
 
-func isFullOverlap(pair string, result chan int) {
+func isFullOverlap(pair string, result chan int, strictOverlap bool) {
 	sections := strings.Split(pair, ",")
 	firstSection := newSection(sections[0])
 	secondSection := newSection(sections[1])
 
-	leftSideOverlap := (firstSection.min <= secondSection.min) && (secondSection.max <= firstSection.max)
-	rightSideOverlap := (secondSection.min <= firstSection.min) && (firstSection.max <= secondSection.max)
-	isFullOverlap := leftSideOverlap || rightSideOverlap
+	var isOverlap bool
+	if strictOverlap {
+		leftSideOverlap := (firstSection.min <= secondSection.min) && (secondSection.max <= firstSection.max)
+		rightSideOverlap := (secondSection.min <= firstSection.min) && (firstSection.max <= secondSection.max)
+		isOverlap = leftSideOverlap || rightSideOverlap
+	} else {
+
+		// (A.min-A.max) - (B.min-B.max)
+		// A.min <= B.min <= A.max <= B.max
+		leftSideIntersect := (secondSection.min <= firstSection.max) && (firstSection.min <= secondSection.max)
+		rightSideIntersect := (firstSection.min <= secondSection.max) && (secondSection.min <= firstSection.max)
+		isOverlap = leftSideIntersect || rightSideIntersect
+	}
 
 	var resultAsInt int
-	if isFullOverlap {
+	if isOverlap {
 		resultAsInt = 1
 	} else {
 		resultAsInt = 0
@@ -45,17 +55,29 @@ func main() {
 	// Fully overlap's simple then
 	// How about a struct as a container?
 
+	// Solution of Part 1
 	sumOfOverlaps := 0
 	for _, pair := range data {
 		result := make(chan int)
-		go isFullOverlap(pair, result)
+		go isFullOverlap(pair, result, true)
 		sumOfOverlaps += <-result
 	}
 
-	fmt.Printf("Overlaps %d", sumOfOverlaps)
+	fmt.Printf("Overlaps %d\n", sumOfOverlaps)
 
 	// I very much believe that the overhead of creating threads (as well as
 	// communicating with them) will not be rewarded in this use case.
 	// However, it is a nice opportunity to familiarize oneself with
 	// Go-routines.
+
+	// Solution of Part 2
+	sumOfOverlapsNonStrict := 0
+	for _, pair := range data {
+		result := make(chan int)
+		go isFullOverlap(pair, result, false)
+		sumOfOverlapsNonStrict += <-result
+	}
+
+	fmt.Printf("Overlaps %d\n", sumOfOverlapsNonStrict)
+
 }
